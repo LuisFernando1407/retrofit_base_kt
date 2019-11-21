299a143ab51201a9bd740b38f7d27a6cf10c49da
LuisFernando1407 <luis1407@live.com>
2019-11-21 15:32:56 -0300

    [DEV] Activity creation if you have an API error

--- app/src/main/java/com/br/base/ui/ServerErrorActivity.kt
+++ app/src/main/java/com/br/base/ui/ServerErrorActivity.kt
@@ -3,11 +3,25 @@ package com.br.base.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.br.base.R
+import kotlinx.android.synthetic.main.activity_server_error.*
+import kotlin.system.exitProcess
 
 class ServerErrorActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_server_error)
+
+        val bundle = intent.extras
+
+        if (bundle != null) {
+            txtMessage.text = bundle.getString("message")
+        }
+    }
+
+    override fun onBackPressed() {
+        moveTaskToBack(true)
+        android.os.Process.killProcess(android.os.Process.myPid())
+        exitProcess(1)
     }
 }
